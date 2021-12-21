e0b15a41b30280fdbacf853fc9ee7f5e66c98b17
Jonathan Bradley <bradley1@vt.edu>
2021-12-21 13:03:10 -0500

    Add promise awaits and start render loop

--- scripts/init.js
+++ scripts/init.js
@@ -1,15 +1,28 @@
 
 const canvas = document.getElementById("renderCanvas");
-const engine = new BABYLON.Engine(canvas, true, {}, true);
+let engine = null;
+let scene = null;
+let sceneToRender = null;
+
+let createDefaultEngine = function() {
+ return new BABYLON.Engine(canvas, true, {
+    preserveDrawingBuffer: true,
+    stencil: true,
+    disableWebGL2Support: false,
+  });
+}
 
 async function createScene(){
     const scene = new BABYLON.Scene(engine);
 
-    let camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+    let camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10),
+    scene);
     camera.setTarget(BABYLON.Vector3.Zero());
     camera.attachControl(canvas, true);
+
     let light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
+    
     let sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
     sphere.position.y = 1;
 
@@ -18,12 +31,45 @@ async function createScene(){
     // here we add XR support
     const xr = await scene.createDefaultXRExperienceAsync({
       floorMeshes: [env.ground],
+      uiOptions: {
+        onError: (error) => {
+          console.log(error);
+        },
+      },
     });
 
     return scene;
 }
 
-(async () => {
-    const finalScene = await createScene();
-})()
- 
+window.initFunction = async function() {  
+  let asyncEngineCreation = async function () {
+    try {
+      return createDefaultEngine();
+    } catch (e) {
+      console.log(
+        "the available createEngine function failed. Creating the default engine instead"
+      );
+      return createDefaultEngine();
+    }
+  };
+
+  window.engine = await asyncEngineCreation();
+  if (!window.engine) throw "engine should not be null.";
+  window.scene = createScene();
+}
+
+window.initFunction().then(() => {
+  window.scene.then((returnedScene) => {
+    sceneToRender = returnedScene;
+  });
+
+  window.engine.runRenderLoop(function () {
+    if (sceneToRender && sceneToRender.activeCamera) {
+      sceneToRender.render();
+    }
+  });
+});
+
+window.addEventListener("resize", function () {
+  window.engine.resize();
+});
